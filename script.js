17e8e24402763cf5f88916da8b8f52ead7b457dc
Azeem <livelyazeem@gmail.com>
2022-04-01 16:27:04 +0500

    still working

--- script.js
+++ script.js
@@ -1,22 +1,5 @@
 function telephoneCheck(str) {
-    let strArr = str.split('');
-    let filteredArr = [];
-    for (let i = 0; i < strArr.length; i++) {
-        if (Boolean(strArr[i].match(/[()\s]/)) == false) {
-            filteredArr.push(strArr[i]);
-        }
-    }
-    console.log(Boolean(...filteredArr.match(/[0-9]/g).length == 10));
-    for (let i of filteredArr) {
-        if (Boolean(i.match(/[0-9-]/)) == false) {
-            return false;
-        }
-    }
-    if (filteredArr[0] == '-' || filteredArr[0] != '1') {
-        return false;
-    }   else if (Boolean(...filteredArr.match(/[0-9]/g).length == 10) == true || Boolean(...filteredArr.match(/[0-9]/g).length == 11) == true){
-        return true;
-    }
+    return /^\s*(?:\+?(1{1,1}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$/.test(str);
 }
 
-console.log(telephoneCheck("(555) 555-5555"));
+console.log(telephoneCheck("27576227382"));
