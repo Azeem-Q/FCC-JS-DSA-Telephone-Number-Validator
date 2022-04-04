e36104178135231a5c894e661c65790416d6409e
Azeem <livelyazeem@gmail.com>
2022-04-04 07:04:22 +0500

    project completed

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function telephoneCheck(str) {
-    return /^\s*(?:\+?(1{1,1}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$/.test(str);
+    let teleRegex = /^1? ?-?\d{3}-? ?\d{3}-? ?\d{4}$|^1? ?-?\(\d{3}\)-? ?\d{3}-? ?\d{4}$/;
+    return teleRegex.test(str);
 }
 
 console.log(telephoneCheck("27576227382"));
